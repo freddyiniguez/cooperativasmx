676cd651184dc3cb537dddd03687853ba7fba42a
Freddy Íñiguez López <iniguez.devt@gmail.com>
2016-06-04 11:11:31 -0500

    General structure of the webapp added.

--- webpage/views.py
+++ webpage/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+# from .models import Cooperativa
 
-# Create your views here.
+def cooperativa_list(request):
+	return render(request, 'webpage/cooperativa_list.html')
