8fe42471808059fa907902d9c23c4167f3cbf727
iscBlanco <iscblanco@gmail.com>
2016-06-04 12:18:58 -0500

    se agrega el modelo

--- webpage/models.py
+++ webpage/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from __future__ import unicode_literals
+from django.db import models 
+from django.utils import timezone
 
-# Create your models here.
+class Trabajador(models.Model):
+	nombre 	= models.TextField()
+	correo 	= models.EmailField()
+	fecha_nac= models.DateTimeField()
+	sexo 	= models.TextField()
+	lugar_origen = models.TextField()
+	habilidades = models.TextField()
