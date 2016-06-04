ad5cab898870c58a4797b17aaef73e7bab8abb23
Freddy Íñiguez López <iniguez.devt@gmail.com>
2016-06-04 16:32:44 -0500

    Fixing view file.

--- webpage/views.py
+++ webpage/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.utils import timezone
-# from .models import Cooperativa
 
 def cooperativa_list(request):
 	return render(request, 'webpage/cooperativa_list.html')
